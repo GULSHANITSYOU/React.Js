4a570d21577b82cff493efc8108a37edc91287d4
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-06-08 18:45:24 +0530

    can't undstant anthing in redux toolkit

---
+++ reduxToolKitToDo/src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h1>Redux Tool kit {"(To Do List)"}</h1>
+    </>
+  )
+}
+
+export default App
