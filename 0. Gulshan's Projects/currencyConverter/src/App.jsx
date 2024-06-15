b5f01d0e667d1dc263281d00edc347769dfa5c08
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-06-15 21:19:59 +0530

    Add tailwind css ans post css config

--- 0. Gulshan's Projects/currencyConverter/src/App.jsx
+++ 0. Gulshan's Projects/currencyConverter/src/App.jsx
@@ -1,22 +1,13 @@
 import { useCallback, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
- const getData = useCallback(()=>{
-  const [data,setdata] = useState(0);
-  setInterval(()=>{
-    setdata( data+1);
-  },1000)
-  return data;
- },[null]);
+
   
 
   return (
     <>
-       <h1>Namste !!   </h1>
+       <h1 className='text-center text-3xl'>Hello</h1>
     </>
   )
 }
